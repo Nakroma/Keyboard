47d93293869180b3da41c1e09fa65734ee3641a4
Nakroma <tarackobarack@gmail.com>
2017-03-04 22:59:44 +0100

    Added threads migration

---
+++ database/migrations/2017_03_04_215340_create_threads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');           // Thread ID
+            $table->unsignedInteger('author');  // Author ID
+            $table->string('topic');            // Thread title
+            $table->string('content');          // Content
+            $table->timestamps();               // Created/Updated at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
