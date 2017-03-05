a39a0abe398205248253e24c1c7232f13cb23fc8
Nakroma <tarackobarack@gmail.com>
2017-03-05 18:34:09 +0100

    Extended thread controller

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -3,6 +3,7 @@
 namespace laravelTest\Http\Controllers;
 
 use Illuminate\Http\Request;
+use laravelTest\Thread;
 
 class ThreadController extends Controller
 {
@@ -14,5 +15,22 @@ class ThreadController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    /**
+     * Show the corresponding thread.
+     *
+     * @param Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        // Pass thread and answers to view
+        $thread = Thread::where('id', $id)->first();
+        // $posts = Post::where('thread_id', $id)->get();
+
+        return view('view_thread', [
+            'thread' => $thread
+        ]);
     }
 }
