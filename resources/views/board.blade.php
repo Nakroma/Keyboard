d0e5deaf0496604b128c694db1d5e3ba39b2fc88
Nakroma <tarackobarack@gmail.com>
2017-03-05 17:35:34 +0100

    Added basic thread display

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -3,14 +3,25 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-2">
 
-                <div class="panel-body">
-                    You are logged in!
+        </div>
+        <div class="col-md-8">
+
+            <!-- Display Threads -->
+            @if (count($threads) > 0)
+                <div class="list-group">
+                    @foreach ($threads as $thread)
+                        <a href="#" class="list-group-item">{{ $thread->topic }}</a>
+                    @endforeach
                 </div>
-            </div>
+            @else
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="alert alert-danger" role="alert">No threads found</div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
