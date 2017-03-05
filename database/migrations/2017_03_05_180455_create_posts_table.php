1e46ddeafbbf136af908321a023f583dcd0700d9
Nakroma <tarackobarack@gmail.com>
2017-03-05 19:06:54 +0100

    Added post migration

---
+++ database/migrations/2017_03_05_180455_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');           // Post ID
+
+            $table->unsignedInteger('author');  // Author ID
+            $table->unsignedInteger('thread');  // Thread ID
+            $table->string('body');             // Content
+
+            $table->timestamps();               // Created/Updated at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
