b841a3a3b33eea0ff8b323d6636e37e849e31388
Nakroma <tarackobarack@gmail.com>
2017-03-09 18:26:10 +0100

    Added ban user form

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,16 @@ use laravelTest\Key;
 class ProfileController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Show the profile.
      *
      * @return \Illuminate\Http\Response
@@ -17,9 +27,16 @@ class ProfileController extends Controller
     {
         $key = Key::where('id', Auth::user()->key_id)->first();
 
+        // Show moderation area depending on the permissions the user has
+        $prm = config('_custom.permissions');
+        $grp = Auth::user()->group;
+        $moderationArea = ($grp >= $prm['banUser'] || $grp >= $prm['promoteUser'] || $grp >= $prm['createKey']);
+
         return view('profile', [
             'user' => Auth::user(),
             'key' => $key,
+            'mod_area' => $moderationArea,
+            'prm' => $prm,
         ]);
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="col-md-8">
 
-                <!-- Thread header -->
+                <!-- Profile -->
                 <div class="panel panel-default">
                     <div class="panel-heading">Profile</div>
                     <div class="panel-body">
@@ -20,6 +20,41 @@
                         <b>Joined in:</b> {{ $user->created_at }}
                     </div>
                 </div>
+
+                <!-- Moderation Area -->
+                @if ($mod_area)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Moderation Area</div>
+                        <div class="panel-body">
+                            @if ($user->group >= $prm['banUser'])
+                                <form class="form-horizontal" role="form" method="POST" action="{{ url('user/ban') }}">
+                                    {{ csrf_field() }}
+
+                                    <div class="form-group" style="text-align: right;">
+                                        <div class="col-md-2">
+                                            <label for="thread" class="control-label">Thread ID</label>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <input id="thread" type="text" class="form-control" name="thread" value="{{ old('thread') }}" required>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <label for="callname" class="control-label">Callname</label>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <input id="callname" type="text" class="form-control" name="callname" value="{{ old('callname') }}" required>
+                                        </div>
+                                        <div class="col-md-3" style="text-align: center;">
+                                            <button type="submit" class="btn btn-danger">
+                                                Ban User
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </form>
+                            @endif
+                        </div>
+                    </div>
+                @endif
 
             </div>
         </div>
